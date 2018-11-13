c6e7ce4bb20e965e31a7892f8f178939d006c3ea
xwp <xwp_fullstack@163.com>
2018-11-13 19:51:46 +0800

    导入静态文件，用模板加载项目首页，favicon图标

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,8 +1,15 @@
+from flask import current_app
+
 from . import news_blue
-from flask import session
+from flask import session,render_template
 
 
 @news_blue.route('/')
 def index():
-    session['name'] = 'qicheng'
-    return 'hello 2019'
+    # session['name'] = 'qicheng'
+    return render_template('news/index.html')
+
+
+@news_blue.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
